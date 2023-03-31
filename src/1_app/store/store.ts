29656bc62e5278c3395e199a5f85b7dc1904be4b
Andrey Peshko <coursestud@mail.ru>
2023-03-31 20:45:54 +0300

    feat: add store

---
+++ src/1_app/store/store.ts
@@ -0,0 +1,36 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './slices/userSlice';
+// import { authApi } from './api/authApi';
+// import { productsApi } from './api/productsApi';
+// import { userApi } from './api/userApi';
+// import loginFormReducer from './slice/loginSlice';
+// import modalReducer from './slice/modalSlice';
+// import { smartphoneApi } from './api/admin/smartphoneApi';
+// import { smartphoneBrandApi } from './api/admin/smartphoneBrandApi';
+// import smartphoneDataSlice from './slice/adminData/smartphoneDataSlice';
+
+export const store = configureStore({
+  reducer: {
+    // loginFormData: loginFormReducer,
+    userData: userReducer,
+    // modal: modalReducer,
+    // smartphoneDataForm: smartphoneDataSlice,
+    // [authApi.reducerPath]: authApi.reducer,
+    // [userApi.reducerPath]: userApi.reducer,
+    // [productsApi.reducerPath]: productsApi.reducer,
+    // [smartphoneApi.reducerPath]: smartphoneApi.reducer,
+    // [smartphoneBrandApi.reducerPath]: smartphoneBrandApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({}).concat([
+      // authApi.middleware,
+      // userApi.middleware,
+      // productsApi.middleware,
+      // smartphoneApi.middleware,
+      // smartphoneBrandApi.middleware,
+    ]),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,16 @@ import '1_app/styles/style.scss';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routes } from './1_app/router/routes';
 import Loader from '7_shared/loader/Loader';
-// import { Provider } from 'react-redux';
-// import { store } from './store/store';
+import { Provider } from 'react-redux';
+import { store } from '1_app/store/store';
 
 const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <RouterProvider router={router} fallbackElement={<Loader />} />
-    {/* </Provider> */} 
+    </Provider> 
   </React.StrictMode>
 );
