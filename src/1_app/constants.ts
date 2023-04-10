8dc2f52b888bd56eb32dd6ab0537d4febdd55c75
Andrey Peshko <coursestud@mail.ru>
2023-04-10 15:08:25 +0300

    feat: add smartphone cart

--- src/1_app/constants.ts
+++ src/1_app/constants.ts
@@ -1,5 +1,6 @@
 export const BASE_URL = 'http://localhost:5050';
 export const DEFAULT_PHOTO_PATH = './avatar.svg';
+export const DEFAULT_IMAGE_NAME = './product.svg';
 
 export const NAME_COOKIE_LOGGED = 'logged_in';
 
---
+++ src/7_shared/utils/helpers.ts
@@ -0,0 +1,3 @@
+export const createRoutePage = (route: string, parameter: string) => {
+  return route + '/' + parameter;
+};
